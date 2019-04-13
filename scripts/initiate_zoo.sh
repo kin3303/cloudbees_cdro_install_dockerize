927e235943329a312b8d8d62f868cae72ad7ac14
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-13 11:29:53 +0900

    Update initiate_zoo.sh

--- scripts/initiate_zoo.sh
+++ scripts/initiate_zoo.sh
@@ -2,19 +2,30 @@
 set -e
 
 export PATH=$PATH:/opt/electriccloud/electriccommander/bin
+
 . /opt/electriccloud/electriccommander/bash.profile
 
-#if [ ! -f /opt/electriccloud/electriccommander/conf/zoo_ready ]; then
-#  echo "sleep 20 senconds to wait zookeeper adnd db start"
+if [ ! -f /opt/electriccloud/electriccommander/conf/server_ready ]; then
+ 
+  echo "sleep 5 senconds to wait zookeeper start"
   sleep 5
   
   cd /opt/electriccloud/electriccommander/conf
+  
   COMMANDER_ZK_CONNECTION=zookeeper1:2181 ../jre/bin/java -jar \
       ../server/bin/zk-config-tool-jar-with-dependencies.jar com.electriccloud.commander.cluster.ZKConfigTool \
       --databasePropertiesFile database.properties \
       --keystoreFile keystore \
       --passkeyFile passkey \
      --commanderPropertiesFile commander.properties
-#fi
+     
+   touch /opt/electriccloud/electriccommander/conf/server_ready
+   
+fi
 
-#touch /opt/electriccloud/electriccommander/conf/zoo_ready
+/etc/init.d/commanderServer restart
+
+while [ ! -f /opt/electriccloud/electriccommander/logs/commander-`hostname`.log ]
+do
+  sleep 2
+done
