d5a38f875ecf760c912979f257b0a49d4bce6922
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-13 23:21:04 +0900

    Update initiate_zoo.sh

--- scripts/initiate_zoo.sh
+++ scripts/initiate_zoo.sh
@@ -5,18 +5,18 @@ export PATH=$PATH:/opt/electriccloud/electriccommander/bin
 
 . /opt/electriccloud/electriccommander/bash.profile
 
-#if [ ! -f /opt/electriccloud/electriccommander/conf/server_ready ]; then
- # echo "sleep 5 senconds to wait zookeeper start"
- # sleep 20
- # cd /opt/electriccloud/electriccommander/conf
- # COMMANDER_ZK_CONNECTION=zookeeper1:2181 ../jre/bin/java -jar \
- #     ../server/bin/zk-config-tool-jar-with-dependencies.jar com.electriccloud.commander.cluster.ZKConfigTool \
- #     --databasePropertiesFile database.properties \
- #     --keystoreFile keystore \
- #     --passkeyFile passkey \
- #    --commanderPropertiesFile commander.properties
- #  touch /opt/electriccloud/electriccommander/conf/server_ready
-#fi
+if [ ! -f /opt/electriccloud/electriccommander/conf/server_ready ]; then
+  echo "sleep 5 senconds to wait zookeeper start"
+  sleep 20
+  cd /opt/electriccloud/electriccommander/conf
+  COMMANDER_ZK_CONNECTION=zookeeper1:2181 ../jre/bin/java -jar \
+      ../server/bin/zk-config-tool-jar-with-dependencies.jar com.electriccloud.commander.cluster.ZKConfigTool \
+      --databasePropertiesFile database.properties \
+      --keystoreFile keystore \
+      --passkeyFile passkey \
+     --commanderPropertiesFile commander.properties
+   touch /opt/electriccloud/electriccommander/conf/server_ready
+fi
 
 /etc/init.d/commanderServer restart
 
