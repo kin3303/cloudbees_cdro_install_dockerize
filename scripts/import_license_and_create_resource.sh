a06a72fbde21c7d509b17cfc0d18c0c69712951e
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-17 00:41:50 +0900

    Update import_license_and_create_resource.sh

--- scripts/import_license_and_create_resource.sh
+++ scripts/import_license_and_create_resource.sh
@@ -6,12 +6,17 @@ export PATH=$PATH:/opt/electriccloud/electriccommander/bin
 ectool login admin changeme
 
 if [ ! -f /opt/electriccloud/electriccommander/conf/license_ready ]; then
-
   ectool importLicenseData /opt/electriccloud/electriccommander/conf/license.xml 
   ectool setProperty "/server/settings/ipAddress" "haproxy"
   ectool setProperty "/server/settings/stompClientUri" "stomp+ssl://haproxy:61613"
   ectool setProperty "/server/settings/stompSecure" "true"
   touch /opt/electriccloud/electriccommander/conf/license_ready
+fi
+
+if [ ! -f /opt/electriccloud/electriccommander/conf/insight_ready ]; then
+  ectool setDevOpsInsightServerConfiguration --userName reportuser --password changeme --enabled 1 \
+  --logStashUrl  https://commanderinsight:9500  --elasticSearchUrl https://commanderinsight:9200 --testConnection 1
+  touch /opt/electriccloud/electriccommander/conf/insight_ready
 fi
 
 if [ ! -f /opt/electriccloud/electriccommander/conf/agents_ready ]; then
@@ -27,7 +32,6 @@ fi
 if [ ! -f /opt/electriccloud/electriccommander/conf/repository_ready ]; then
   ectool createRepository myRepo --repositoryDisabled "false" --url  "https://repository:8200" 
   touch /opt/electriccloud/electriccommander/conf/repository_ready
-  
 fi
 
 
