2572569624fe2f31339f5650b4656e3e2b7a7463
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-16 23:00:02 +0900

    Update download.sh

--- setup/download.sh
+++ setup/download.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+defaultRepo="https://github.com/kin3303/efdemo"
+
+BRANCH=${1:-master} 
+REMOTE_REPO=${2:-$defaultRepo}
+
+
+localFolder="/Users/myname/dev/myfolder"
+mkdir $BRANCH
+cd $BRANCH
+
+git init
+git remote add -t $BRANCH -f origin $REMOTE_REPO
+git checkout $BRANCH
