f2b2e77e7b0b3d0b74ae6677be30fb4e8f643880
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-17 16:42:41 +0900

    Update start.sh

--- setup/start.sh
+++ setup/start.sh
@@ -4,3 +4,5 @@ tag=${1:-latest}
 make cleankeepdata
 TAG="${tag}" docker-compose up -d
 docker-compose logs -f
+ipAddress="$(dig @resolver1.opendns.com ANY myip.opendns.com +short)"
+echo "http://$ipAddress:1936/haproxy?stats"
