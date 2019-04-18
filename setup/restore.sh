1d6d3e2e3d7af733e1400143836108ff2238f82f
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-19 02:10:19 +0900

    Update restore.sh

--- setup/restore.sh
+++ setup/restore.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
-#mysql
+###################################################################################################
+# DB Restore
+###################################################################################################
 sudo docker exec -it $(docker ps |grep db_1|awk '{print $1}') /bin/bash /tmp/scripts/restoredb.sh
 
-#plugin
+restoreDir=data/db-data/backup/ecdb
 
-#logs
+rm -rf $restoreDir/*
 
-#repository-data
 
-#workspace
+unzip 'abc*.zip'
+if [ !-d "backup.tar.xz" ]; then
+   echo "Restore Failed - There is no backup.tar.xz";
+   exit 1;
+fi
 
-#elasticSearch
+if [ !-d "$restoreDir" ]; then
+    echo "Backup Failed - There is no backup directory made by DB backup process";
+    exit 1;
+fi
+
+###################################################################################################
+# Data Restore
+#    Configuration Files
+#       <DATADIR>/conf  :  Configuration files for the Server and  Artifact Repository
+#       <DATADIR>/mysql/mysql.cnf  : Configuration file for MYSQL
+#       <DATADIR>/apache/conf  : Configuration files for the Web Server => SKIPED 
+#    Workspace Directories
+#       <DATADIR>/workspace
+#    Plugins
+#       <DATADIR>/plugins
+#    Artifact
+#       <DATADIR>/repository-data
+###################################################################################################
+cp -r data/conf $backupDir
+cp -r data/mysql  $backupDir
+cp -r data/workspace $backupDir
+cp -r data/plugins $backupDir
+cp -r data/repository-data $backupDir
  
+###################################################################################################
+# Packaging
+###################################################################################################
+date=$(date +%F)
+
+tar cvfj backup.tar.xz  $backupDir
+tar tvf backup.tar.xz
