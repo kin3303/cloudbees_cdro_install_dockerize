3f2149cfb0bf8fe4f96894a6874a7aa296f1c9f7
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-18 15:22:34 +0900

    Update backup.sh

--- setup/backup.sh
+++ setup/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #mysql
-docker exec $(docker ps |grep db|awk '{print $1}')  /tmp/scripts/backupdb.sh
+docker exec $(docker ps |grep db|awk '{print $1}') /bin/bash /tmp/scripts/backupdb.sh
 
 #plugin
 
