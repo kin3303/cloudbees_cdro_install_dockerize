4fd9c46345a3986e8c46987059c3c30cc60a4224
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-18 22:06:32 +0900

    Update install.sh

--- scripts/sqlbackup/install.sh
+++ scripts/sqlbackup/install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-set -e
 
-chmod 755 mysqlbkup.sh 
+# Deploy the executable
 cp mysqlbkup.sh /usr/local/bin
 chmod 755 /usr/local/bin/mysqlbkup.sh
 
-cp mysqlbkup.config /etc/mysqlbkup.config
+# Deploy the configuration files
+cp mysqlbkup.config.sample /etc/mysqlbkup.config
 chmod 600 /etc/mysqlbkup.config
- 
+cp mysqlbkup.cnf.sample /etc/mysqlbkup.cnf
+chmod 600 /etc/mysqlbkup.cnf
+
+# Create the backup directory
 . /etc/mysqlbkup.config
 mkdir "$BACKUP_DIR"
 chmod 600 "$BACKUP_DIR"
