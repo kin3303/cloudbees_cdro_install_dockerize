dba676a078d0c3c175fb3d89e2ef60d7d3da6c8b
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-18 16:03:44 +0900

    Update install.sh

--- scripts/sqlbackup/install.sh
+++ scripts/sqlbackup/install.sh
@@ -7,16 +7,8 @@ chmod 755 /usr/local/bin/mysqlbkup.sh
 
 cp mysqlbkup.config /etc/mysqlbkup.config
 chmod 600 /etc/mysqlbkup.config
-cp mysqlbkup.cnf /etc/mysqlbkup.cnf
-chmod 600 /etc/mysqlbkup.cnf
+ 
 
 . /etc/mysqlbkup.config
 mkdir "$BACKUP_DIR"
 chmod 600 "$BACKUP_DIR"
-
-echo "mysqlbkup is installed"
-echo "Make sure to edit /etc/mysqlbkup.cnf and /etc/mysqlbkup.config for your needs"
-
-echo "Lastly, configure a crontab to execute the backup script periodically"
-echo "For example, to schedule the script to run once daily"
-echo "1 2 * * * /usr/local/bin/mysqlbkup.sh 1>> /var/log/mysqlbkup.log 2>>/var/log/mysqlbkup-err.log"
