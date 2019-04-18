3c27b8af56c7991c5ae7686e0da459414b23eeb3
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-18 15:22:49 +0900

    Update restore.sh

--- setup/restore.sh
+++ setup/restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #mysql
-docker exec $(docker ps |grep db_1|awk '{print $1}')  /tmp/scripts/restoredb.sh
+docker exec $(docker ps |grep db|awk '{print $1}') /bin/bash /tmp/scripts/restoredb.sh
 
 #plugin
 
