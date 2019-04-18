21fb02d492a0abfaf47c2eb85d2e0182f11c849b
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-18 22:22:08 +0900

    Update backupdb.sh

--- scripts/backupdb.sh
+++ scripts/backupdb.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-./tmp/scripts/sqlbackup/install.sh
-./tmp/scripts/sqlbackup/mysqlbkup.sh
+
+cd /tmp/scripts/sqlbackup/
+./install.sh
+/usr/local/bin/mysqlbkup.sh 1>> /var/log/mysqlbkup.log 2>>/var/log/mysqlbkup-err.log
+
+echo "-------------Error Log -----------------------------------------"
+echo ""
+cat /var/log/mysqlbkup-err.log
+echo ""
+echo "-------------Backup Log -----------------------------------------"
+echo ""
+cat /var/log/mysqlbkup.log
+echo ""
