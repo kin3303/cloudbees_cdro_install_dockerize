8b2fa8ccf9c230dc0343ee58ee906cc36f943b50
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-18 14:48:01 +0900

    Update and rename backupdb.sh to backup.sh

--- setup/backup.sh
+++ setup/backup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+#mysql db backup
 docker exec $(docker ps |grep db_1|awk '{print $1}')  /tmp/scripts/backupdb.sh
+
+#plugin directory backup
+
+#logs backup
+
+
+#repository-data backup
  
