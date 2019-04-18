1d91fefd48106364cc2829b39876f5b3542e47cb
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-18 16:30:36 +0900

    Update backupdb.sh

--- scripts/backupdb.sh
+++ scripts/backupdb.sh
@@ -1,6 +1,7 @@
- cd /tmp/scripts/sqlbackup/
- apt-get update
- chmod +x install.sh
- chmod +x mysqlbkup.sh
- ./install.sh
- ./mysqlbkup.sh
+#!/bin/bash
+cd /tmp/scripts/sqlbackup/
+apt-get update
+chmod +x install.sh
+chmod +x mysqlbkup.sh
+./install.sh
+./mysqlbkup.sh
