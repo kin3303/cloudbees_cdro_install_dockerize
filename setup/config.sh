01501b5753a6347f3f67cec46d459fe17f312427
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-18 13:21:10 +0900

    Update config.sh

--- setup/config.sh
+++ setup/config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cp /tmp/license.xml ./data/conf/
-
 docker exec $(docker ps |grep commanderserver_1|awk '{print $1}')  /tmp/scripts/import_license_and_create_resource.sh
+rm -rf ./data/conf/license.xml
