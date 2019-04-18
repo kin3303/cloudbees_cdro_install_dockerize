68394c86d8a609c1075ca1a34da7a2ff38cacc66
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-19 01:55:49 +0900

    Update backup.sh

--- setup/backup.sh
+++ setup/backup.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-#mysql
+###################################################################################################
+# DB Backup
+###################################################################################################
 sudo docker exec -it $(docker ps |grep db_1|awk '{print $1}') /bin/bash /tmp/scripts/backupdb.sh
+backupDir=data/db-data/backup/ecdb
+if [ !-d "$backupDir" ]; then
+    echo "Backup Failed - There is no backup directory made by DB backup process";
+    exit 1;
+fi
 
-#plugin
-
-#logs
-
-#repository-data
-
-#workspace
-
-#elasticSearch
+###################################################################################################
+# Data Backup
+#    Configuration Files
+#       <DATADIR>/conf  :  Configuration files for the Server and  Artifact Repository
+#       <DATADIR>/mysql/mysql.cnf  : Configuration file for MYSQL
+#       <DATADIR>/apache/conf  : Configuration files for the Web Server => SKIPED 
+#    Workspace Directories
+#       <DATADIR>/workspace
+#    Plugins
+#       <DATADIR>/plugins
+#    Artifact
+#       <DATADIR>/repository-data
+###################################################################################################
+cp -r data/conf $backupDir
+cp -r data/mysql  $backupDir
+cp -r data/workspace $backupDir
+cp -r data/plugins $backupDir
+cp -r data/repository-data $backupDir
  
+###################################################################################################
+# Packaging
+###################################################################################################
+tar cvfj backup.tar.xz  $backupDir
