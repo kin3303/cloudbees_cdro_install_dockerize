7cc222deaa021ecece6c6247e77c3d501db50731
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-20 13:03:45 +0900

    Update import_license_and_create_resource.sh

--- scripts/import_license_and_create_resource.sh
+++ scripts/import_license_and_create_resource.sh
@@ -38,9 +38,9 @@ fi
 
 if [ ! -f /opt/electriccloud/electriccommander/conf/demo_ready ]; then 
   for file in /tmp/scripts/pluginResources/*.jar; do
-    #ectool installPlugin "$file"
-    project_name=$(ectool installPlugin "$file" | ec-perl -e 'use XML::Simple; print XMLin("-")->{plugin}->{projectName};')
-    ectool promotePlugin "${project_name}"
+    ectool installPlugin "$file"
+    #project_name=$(ectool installPlugin "$file" | ec-perl -e 'use XML::Simple; print XMLin("-")->{plugin}->{projectName};')
+    #ectool promotePlugin "${project_name}"
   done
   touch /opt/electriccloud/electriccommander/conf/demo_ready
 fi
