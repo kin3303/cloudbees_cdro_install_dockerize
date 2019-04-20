0cae8983daf960fd51f2b39c2bbb7c4bfd04266f
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-20 23:02:12 +0900

    Update backupel.sh

--- scripts/backupel.sh
+++ scripts/backupel.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 cd /tmp/scripts/elasticsearchbackup/
-
-#sbkup.sh /usr/local/bin
-chmod 755 esbkup.sh
-
+ 
 ./esbkup.sh 1>> /var/log/esbkup.log 2>>/var/log/esbkup-err.log
 
 echo "-------------Error Log -----------------------------------------"
