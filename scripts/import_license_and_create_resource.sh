4856f91bf0703a340ce01e0c039a60f24016f80a
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-20 09:17:19 +0900

    Update import_license_and_create_resource.sh

--- scripts/import_license_and_create_resource.sh
+++ scripts/import_license_and_create_resource.sh
@@ -16,6 +16,8 @@ fi
 if [ ! -f /opt/electriccloud/electriccommander/conf/agents_ready ]; then
   ectool createResource local --hostName localagent --pools default
   ectool pingResource local
+  ectool createResource localhost --hostName localagent --pools default
+  ectool pingResource localhost
   ectool createResource apacheAgent --hostName  commanderapache 
   ectool pingResource apacheAgent
   ectool createResource repositoryAgent --hostName  repository 
