48352f86ed9b13d0b223af3ca15e1cc54e692cd1
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-04-22 15:57:08 +0900

    Update esrestore.sh

--- scripts/elasticsearchbackup/esrestore.sh
+++ scripts/elasticsearchbackup/esrestore.sh
@@ -2,4 +2,21 @@
 cd /usr/share/elasticsearch/backup
 tar xzvf elasticsearch-backup.tar.gz
 
+########################################################
+# Add the required path.repo to elasticsearch yaml file
+########################################################
+if [ ! -f /opt/electriccloud/electriccommander/conf/reporting/elasticsearch/repository_ready ]; then
+cat >> /opt/electriccloud/electriccommander/conf/reporting/elasticsearch/elasticsearch.yml << EOF
+path.repo: ["$snapshotdir"]
+EOF
+touch /opt/electriccloud/electriccommander/conf/reporting/elasticsearch/repository_ready
+
+fi 
+
+/etc/init.d/commanderElasticsearch restart 
+sleep 10
+
+########################################################
+# Restore 
+########################################################
 curl -k -X POST "localhost:9200/_snapshot/my_backup/insight/_restore?wait_for_completion=true"
