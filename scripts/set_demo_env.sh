454f5b24225fb75b28b4904a4098de82e5f5ceea
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-05-21 23:53:14 +0900

    Update set_demo_env.sh

--- scripts/set_demo_env.sh
+++ scripts/set_demo_env.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
+set -e
 
-AGENT_SCRIPT_FILE=create_agents.sh
-echo "#!/bin/bash" > $AGENT_SCRIPT_FILE
+export PATH=$PATH:/opt/electriccloud/electriccommander/bin
 
-echo 'export PATH=$PATH:/opt/electriccloud/electriccommander/bin' >> $AGENT_SCRIPT_FILE
-echo 'ectool login admin changeme' >>  $AGENT_SCRIPT_FILE
+ectool login admin changeme
 
-for i in $(seq 1 $(docker ps |grep localagent | wc -l))
-do
-    AGENT_HOST=$(docker ps|grep "localagent_$i$"|awk '{print $1}')
-    echo $AGENT_HOST
-    echo "ectool deleteResource  local$i"  >>  $AGENT_SCRIPT_FILE
-    echo "ectool createResource local$i --hostName $AGENT_HOST --pools pool$(expr $i % 5)" >> $AGENT_SCRIPT_FILE
-    echo "ectool pingResource local$i" >> $AGENT_SCRIPT_FILE
+echo "Install Plugins"
+for file in /tmp/scripts/pluginResources/*.jar; do
+  fileName=$(basename "$file")
+  if [ $fileName = "ReleaseDemo.jar" ] ; then
+    ectool installPlugin "$file"
+  else
+    ectool promotePlugin `ectool installPlugin $file --force true | grep -oPm1 "(?<=<pluginName>)[^<]+"`
+  fi
 done
 
-docker exec $(docker ps |grep commanderserver_1|awk '{print $1}')   /tmp/scripts/create_agents.sh
+echo "Import Projects"
+for file in /tmp/scripts/projectResouces/*.xml; do
+  ectool import --file "$file" --force 1
+done
+
+echo "Creating groups"
+for i in administrators development quality release operations executive it; do
+  ectool createGroup $i
+done
+
+echo "Creating users"
+ectool createUser anne --password changeme --fullUserName "Administrator Anne" --groupNames administrators --email "anne@flow.localdomain"
+ectool createUser dave --password changeme --fullUserName "Developer Dave" --groupNames development --email "dave@flow.localdomain"
+ectool createUser quinn --password changeme --fullUserName "Quality Quinn" --groupNames quality --email "quinn@flow.localdomain"
+ectool createUser raj --password changeme --fullUserName "Releaser Raj" --groupNames release --email "raj@flow.localdomain"
+ectool createUser oscar --password changeme --fullUserName "Operations Oscar" --groupNames operations --email "oscar@flow.localdomain"
+ectool createUser eddie --password changeme --fullUserName "Executive Eddie" --groupNames executive --email "eddie@flow.localdomain"
+ectool createUser ingrid --password changeme --fullUserName "IT Ingrid" --groupNames it --email "ingrid@flow.localdomain"
+
+echo "Disable Sentry Monitor"
+ectool --silent  modifySchedule "Electric Cloud" ECSCM-SentryMonitor --scheduleDisabled true
+
+echo "Setting top-level security policies"
+ectool --silent setProperty "/server/flow_demo/security_policy" --valueFile  /tmp/scripts/projectResouces/policy.json 
+ectool --silent runProcedure "/plugins/EC-Security/project" --procedureName ApplyPolicy --actualParameter "policyLocation=/server/flow_demo/security_policy" --pollInterval 1
 
