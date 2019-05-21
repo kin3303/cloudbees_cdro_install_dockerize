540137b19f99689b7ad97095e451a94f1f2675b9
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-05-21 23:57:39 +0900

    Update demo_config.sh

--- setup/demo_config.sh
+++ setup/demo_config.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cp /tmp/license.xml ./data/conf/
-docker exec $(docker ps |grep commanderserver_1|awk '{print $1}')  /tmp/scripts/import_license_and_create_resource.sh
-rm -rf ./data/conf/license.xml
+docker exec $(docker ps |grep commanderserver_1|awk '{print $1}')  /tmp/scripts/set_demo_env.sh
