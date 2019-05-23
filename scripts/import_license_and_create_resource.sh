ea7ac319ccb66737ddb9ba0a997ad12d5e65d499
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-05-23 10:46:49 +0900

    Update import_license_and_create_resource.sh

--- scripts/import_license_and_create_resource.sh
+++ scripts/import_license_and_create_resource.sh
@@ -23,8 +23,8 @@ if [ ! -f /opt/electriccloud/electriccommander/conf/agents_ready ]; then
   ectool createResource repositoryAgent --hostName  repository 
   ectool pingResource repositoryAgent
   
-  ectool createResource appAgent --hostName  webagent
-  ectool pingResource appAgent
+  ectool createResource webAgent --hostName  webagent
+  ectool pingResource webAgent
   
   ectool createResource dbAgent --hostName  dbagent
   ectool pingResource dbAgent
