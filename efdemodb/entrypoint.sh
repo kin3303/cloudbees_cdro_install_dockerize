99beb9c29317e8049b60214d662fdf08478b4d8b
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-05-24 16:45:56 +0900

    Update entrypoint.sh

--- efdemodb/entrypoint.sh
+++ efdemodb/entrypoint.sh
@@ -2,9 +2,10 @@
 
 /etc/init.d/commanderAgent start
  
-
+# Install database file
 mysql_install_db --user mysql > /dev/null
 
+# Create changing root password sql file
 cat > /tmp/sql <<EOF
 USE mysql;
 FLUSH PRIVILEGES;
@@ -12,7 +13,9 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 UPDATE user SET password=PASSWORD("1234") WHERE user='root';
 EOF
 
+# Change root password
 mysqld --bootstrap --verbose=0 < /tmp/sql
 rm -rf /tmp/sql
 
+# Run mysql
 mysqld
