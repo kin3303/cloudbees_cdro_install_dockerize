9c5d93aac574f5d5244f24facc3479ad09619159
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-09-17 16:24:19 +0900

    Update initialize_db.sh

--- scripts/initialize_db.sh
+++ scripts/initialize_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mysqladmin -u ecdb password ecdb
 
-mysql --user=ecdb --password=ecdb <<EOF
+mysql --user=root --password=ecdb <<EOF
 drop database if exists univers;
 create database univers;
-grant all on *.* to ecdb@localhost IDENTIFIED BY 'ecdb';
+grant all on *.* to root@localhost IDENTIFIED BY 'ecdb';
 use univers;
 EOF
