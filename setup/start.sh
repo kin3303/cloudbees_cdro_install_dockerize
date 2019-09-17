6808a3779f7390dc55c4f2eee43573100b1e02f7
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-09-17 12:21:28 +0900

    Update start.sh

--- setup/start.sh
+++ setup/start.sh
@@ -3,7 +3,7 @@
 tag=${1:-latest} 
 file=${2:-docker-compose.yaml} 
 make cleankeepdata
-TAG="${tag}" docker-compose up -d ${file}
+TAG="${tag}" docker-compose -f ${file} up -d
 docker-compose logs -f
 ipAddress="$(dig @resolver1.opendns.com ANY myip.opendns.com +short)"
 echo ""
