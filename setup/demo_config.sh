b733adea7456f77b10578926652a71f5bf10bda8
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-09-18 13:38:34 +0900

    Update demo_config.sh

--- setup/demo_config.sh
+++ setup/demo_config.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 docker exec $(docker ps |grep commanderserver|awk '{print $1}')  /tmp/scripts/set_demo_env.sh
-
-docker exec $(docker ps |grep mysql|awk '{print $1}')  /tmp/scripts/initialize_db.sh
