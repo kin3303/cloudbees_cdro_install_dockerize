78e7df641e2154acabe472bfd4a821c0e1c59844
root <root@instance-1.asia-northeast1-b.c.oceanic-grin-249600.internal>
2019-09-18 04:25:44 +0000

    update soruces

---
+++ efdemoweb3/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#if [ "$INIT" = "1" ]; then
+        echo "Initializing $DATADIR..."
+        mysql_install_db
+#fi
+
+echo "Checking integrity of data from $DATADIR..."
+/usr/sbin/mysqld &
+TMPPID=$!
+sleep 15
+
+mysql --user=root --password=password "SHOW databases; USE mysql;"
+
+mysqladmin -u root password 'demodb'
+
+kill -TERM $TMPPID && wait
+STATUS=$?
+
+if [ "$STATUS" = "0" ]; then
+        echo "Using data from $DATADIR..."
+        /etc/init.d/mysql start && /etc/init.d/commanderAgent start && tail -F /opt/electriccloud/electriccommander/logs/agent/agent.log
+else
+        echo "Cannot load data from $DATADIR."
+fi
