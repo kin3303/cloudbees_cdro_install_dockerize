71bedf1600de5f77428686c6a25ff3daa0c71bf9
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-09-22 02:32:15 +0900

    Update entrypoint.sh

--- efdemodb/entrypoint.sh
+++ efdemodb/entrypoint.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-#if [ "$INIT" = "1" ]; then
-        echo "Initializing $DATADIR..."
-        mysql_install_db
-#fi
+if [ ! -f /tmp/mysql_ready ]; then
+  echo "Initializing $DATADIR..."
+  mysql_install_db
+  touch /tmp/mysql_ready
+fi
 
 echo "Checking integrity of data from $DATADIR..."
 /usr/sbin/mysqld &
 TMPPID=$!
-sleep 15
+sleep 30
 
-mysql --user=root --password=password "SHOW databases; USE mysql;"
 
-mysqladmin -u root password 'demodb'
+mysql --user=root --password=root "SHOW databases; USE mysql;"
+
+mysqladmin -u root password 'password'
+
+mysql --user=root --password=password  <<EOF
+drop database if exists univers;
+create database univers;
+grant all privileges on *.* to 'root'@'%' identified by 'password'; 
+flush privileges;
+use univers;
+EOF
 
 kill -TERM $TMPPID && wait
 STATUS=$?
 
 if [ "$STATUS" = "0" ]; then
         echo "Using data from $DATADIR..."
+        echo "Starting mysql services.."
         /etc/init.d/mysql start && /etc/init.d/commanderAgent start && tail -F /opt/electriccloud/electriccommander/logs/agent/agent.log
 else
         echo "Cannot load data from $DATADIR."
