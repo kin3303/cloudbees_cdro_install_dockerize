b9bbd1aefcbe2f487788f718172d973fa00d9461
root <root@instance-1.asia-northeast1-b.c.oceanic-grin-249600.internal>
2019-09-21 17:14:50 +0000

    update sources

---
+++ efdemoprod/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ ! -f /tmp/mysql_ready ]; then
+  echo "Initializing $DATADIR..."
+  mysql_install_db
+  touch /tmp/mysql_ready
+fi
+
+echo "Checking integrity of data from $DATADIR..."
+/usr/sbin/mysqld &
+TMPPID=$!
+sleep 30
+
+
+mysql --user=root --password=root "SHOW databases; USE mysql;"
+
+mysqladmin -u root password 'password'
+
+mysql --user=root --password=password  <<EOF
+drop database if exists univers;
+create database univers;
+grant all privileges on *.* to 'root'@'%' identified by 'password'; 
+flush privileges;
+use univers;
+EOF
+
+kill -TERM $TMPPID && wait
+STATUS=$?
+
+if [ "$STATUS" = "0" ]; then
+        echo "Using data from $DATADIR..."
+        echo "Starting mysql and wildfly services.."
+        /etc/init.d/mysql start && /etc/init.d/wildfly start && /etc/init.d/commanderAgent start && tail -F /opt/electriccloud/electriccommander/logs/agent/agent.log
+else
+        echo "Cannot load data from $DATADIR."
+fi
