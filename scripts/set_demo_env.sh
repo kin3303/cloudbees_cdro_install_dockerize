b2f590976d58a01c879c1d4f29b9edaebf309b4d
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-09-23 02:46:58 +0900

    Update set_demo_env.sh

--- scripts/set_demo_env.sh
+++ scripts/set_demo_env.sh
@@ -5,49 +5,65 @@ export PATH=$PATH:/opt/electriccloud/electriccommander/bin
 
 ectool login admin changeme
 
-if [ ! -f /opt/electriccloud/electriccommander/conf/demo_agents_ready ]; then
-
+if [ ! -f /opt/electriccloud/electriccommander/conf/demo_prod_agents_ready ]; then
   ectool createResource PROD --hostName  prodagent 
   ectool pingResource PROD
-  
-  ectool createResource DEV --hostName  devagent 
-  ectool pingResource DEV
-  
-  ectool createResource QA --hostName  qaagent 
-  ectool pingResource QA
-  
-  ectool createResource DB --hostName  dbagent 
-  ectool pingResource DB 
-  
-  
-  touch /opt/electriccloud/electriccommander/conf/demo_agents_ready
+  touch /opt/electriccloud/electriccommander/conf/demo_prod_agents_ready
 fi
 
-echo "Install Plugins"
-for file in /tmp/scripts/pluginResources/*.jar; do
-  fileName=$(basename "$file")
-  if [ $fileName = "ReleaseDemo.jar" ] ; then
-    ectool installPlugin "$file"
-  else
-    ectool promotePlugin `ectool installPlugin $file --force true | grep -oPm1 "(?<=<pluginName>)[^<]+"`
-  fi
-done
+if [ ! -f /opt/electriccloud/electriccommander/conf/demo_dev_agents_ready ]; then
+  ectool createResource DEV --hostName  devagent 
+  ectool pingResource DEV
+  touch /opt/electriccloud/electriccommander/conf/demo_dev_agents_ready
+fi
 
-echo "Import Projects"
-for file in /tmp/scripts/projectResouces/*.xml; do
-  ectool import --file "$file" --force 1
-  
-  fileName=$(basename "$file")
-  projectName=${fileName%.*}
- 
-  ectool createAclEntry user "project: $projectName" --systemObjectName server --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
-  ectool createAclEntry user "project: $projectName" --systemObjectName projects --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
-  ectool createAclEntry user "project: $projectName" --systemObjectName resources --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
-  ectool createAclEntry user "project: $projectName" --systemObjectName artifacts --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
-  ectool createAclEntry user "project: $projectName" --systemObjectName repositories --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
-  ectool createAclEntry user "project: $projectName" --systemObjectName session --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
-  ectool createAclEntry user "project: $projectName" --systemObjectName workspaces --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
-done
+if [ ! -f /opt/electriccloud/electriccommander/conf/demo_qa_agents_ready ]; then
+  ectool createResource QA --hostName  qaagent 
+  ectool pingResource QA
+  touch /opt/electriccloud/electriccommander/conf/demo_qa_agents_ready
+fi
+
+if [ ! -f /opt/electriccloud/electriccommander/conf/demo_db_agents_ready ]; then
+  ectool createResource DB --hostName  dbagent 
+  ectool pingResource DB 
+  touch /opt/electriccloud/electriccommander/conf/demo_db_agents_ready
+fi
+
+
+if [ ! -f /opt/electriccloud/electriccommander/conf/plugin_install_ready ]; then
+  echo "Install Plugins"
+  for file in /tmp/scripts/pluginResources/*.jar; do
+    fileName=$(basename "$file")
+    if [ $fileName = "ReleaseDemo.jar" ] ; then
+      ectool installPlugin "$file"
+    else
+      ectool promotePlugin `ectool installPlugin $file --force true | grep -oPm1 "(?<=<pluginName>)[^<]+"`
+    fi
+  done
+
+  touch /opt/electriccloud/electriccommander/conf/plugin_install_ready
+fi
+
+
+if [ ! -f /opt/electriccloud/electriccommander/conf/project_import_ready ]; then
+  echo "Import Projects"
+  for file in /tmp/scripts/projectResouces/*.xml; do
+    ectool import --file "$file" --force 1
+
+    fileName=$(basename "$file")
+    projectName=${fileName%.*}
+
+    ectool createAclEntry user "project: $projectName" --systemObjectName server --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
+    ectool createAclEntry user "project: $projectName" --systemObjectName projects --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
+    ectool createAclEntry user "project: $projectName" --systemObjectName resources --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
+    ectool createAclEntry user "project: $projectName" --systemObjectName artifacts --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
+    ectool createAclEntry user "project: $projectName" --systemObjectName repositories --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
+    ectool createAclEntry user "project: $projectName" --systemObjectName session --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
+    ectool createAclEntry user "project: $projectName" --systemObjectName workspaces --executePrivilege allow --readPrivilege allow --modifyPrivilege allow --changePermissionsPrivilege allow
+  done
+
+  touch /opt/electriccloud/electriccommander/conf/project_import_ready
+fi
 
 echo "Creating groups"
 for i in administrators development quality release operations executive it; do
@@ -59,7 +75,6 @@ ectool --silent  modifySchedule "Electric Cloud" ECSCM-SentryMonitor --scheduleD
 
 echo "Plugin Modification"
 ectool --silent  modifyProject "/plugins/EC-JIRA/project" --resourceName "local" --workspaceName "default"
-
 
 if [ ! -f /opt/electriccloud/electriccommander/conf/demo_creds_ready ]; then
   echo "Setting JBoss credentials..."
@@ -83,6 +98,3 @@ if [ ! -f /opt/electriccloud/electriccommander/conf/demo_creds_ready ]; then
   
   touch /opt/electriccloud/electriccommander/conf/demo_creds_ready
 fi
-
-
-
