0f8d60ad35e9b8e3d67968fdf19cf7ca6be9b43e
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-09-25 23:22:06 +0900

    Update set_demo_env.sh

--- scripts/set_demo_env.sh
+++ scripts/set_demo_env.sh
@@ -69,3 +69,17 @@ ectool --silent  modifySchedule "Electric Cloud" ECSCM-SentryMonitor --scheduleD
 
 echo "Plugin Modification"
 ectool --silent  modifyProject "/plugins/EC-JIRA/project" --resourceName "local" --workspaceName "default"
+
+if [ ! -f /opt/electriccloud/electriccommander/conf/demo_creds_ready ]; then
+  echo "Setting JBoss credentials..."
+  ectool --silent runProcedure /plugins/EC-JBoss/project --procedureName CreateConfiguration \
+       --actualParameter config=jbosscfg jboss_url="localhost:9990" scriptphysicalpath=/opt/jboss/wildfly/bin/jboss-cli.sh --pollInterval 30
+  ectool --silent modifyCredential /plugins/EC-JBoss/project jbosscfg --userName admin --password changeme
+
+  echo "Setting MySQL credentials..."
+  ectool --silent runProcedure /plugins/EC-MYSQL/project --procedureName CreateConfiguration \
+       --actualParameter config=mysqlcfg --pollInterval 30
+  ectool --silent modifyCredential /plugins/EC-MYSQL/project mysqlcfg --userName root --password password
+ 
+  touch /opt/electriccloud/electriccommander/conf/demo_creds_ready
+fi
