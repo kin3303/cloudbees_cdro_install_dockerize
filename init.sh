2483bc5926611bae8f1f038caf1c1c77b7f4d8bb
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-03-19 14:02:19 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -20,7 +20,5 @@ chmod 777 data/*
 echo -e "alias cbfInstallPackage='bash ./setup/install.sh'" >> ~/.bashrc
 echo -e "alias cbfInstallCBF='bash ./setup/start.sh'" >> ~/.bashrc
 echo -e "alias cbfConfigEnv='bash ./setup/config.sh'" >> ~/.bashrc
-echo -e "alias cbfsetdemo='bash ./setup/demo_config.sh'" >> ~/.bashrc
-echo -e "alias cbfstop='bash ./setup/stop.sh'" >> ~/.bashrc  
-echo -e "alias cbfupload='bash ./upload.sh'" >> ~/.bashrc 
+echo -e "alias cbfSetDemo='bash ./setup/demo_config.sh'" >> ~/.bashrc  
 
