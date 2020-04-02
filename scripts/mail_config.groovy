ef27ee7209407d3f53f891612307c7d2b5c248b7
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-04-02 14:41:35 +0900

    Update mail_config.groovy

--- scripts/mail_config.groovy
+++ scripts/mail_config.groovy
@@ -1,13 +1,9 @@
-{
-  "emailConfigs" : [
-    { "name" : "gmail",
-      "description" : "gmail account access in EF",
-      "mailFrom" : "noReply@gmail.com",
-      "mailHost" : "smtp.gmail.com",
-      "mailPort" : "465",
-      "mailProtocol" : "SMTPS",
-      "mailUser" : ""
-      "mailUserPassword" : ""
-    }
-  ]
+emailConfig "gmailTest", {
+      description = "gmail account access in EF"
+      mailFrom = "noReply@gmail.com"
+      mailHost = "smtp.gmail.com"
+      mailPort = "465"
+      mailProtocol = "SMTPS"
+      mailUser = "kin3303@gmail.com"
+      mailUserPassword = "!QA2ws#ED4rf"
 }
