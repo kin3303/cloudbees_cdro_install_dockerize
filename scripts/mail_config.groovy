1e2aee047950436135bcf381f51a077a8f83ca56
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-04-02 15:21:37 +0900

    Delete mail_config.groovy

--- scripts/mail_config.groovy
+++
@@ -1,9 +0,0 @@
-emailConfig "gmailTest", {
-      description = "gmail account access in EF"
-      mailFrom = "noReply@gmail.com"
-      mailHost = "smtp.gmail.com"
-      mailPort = "465"
-      mailProtocol = "SMTPS"
-      mailUser = "kin3303@gmail.com"
-      mailUserPassword = "!QA2ws#ED4rf"
-}
